80baf58892a223a4e108bbf954c8a2122e8fa54e
big-javascript-fan <wdeveloping9099@gmail.com>
2021-11-08 10:25:47 -0500

    config hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,40 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const { mnemonic, apiKey, infuraKey } = require('./secrets.json');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+	solidity: {
+		version: "0.8.0",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 10
+			}
+		}
+	},
+	networks: {
+		rinkeby: {
+			url: 'https://rinkeby.infura.io/v3/' + infuraKey,
+			gas: 10000000,
+			accounts: { mnemonic: mnemonic }
+		},
+		ropsten: {
+			url: 'https://ropsten.infura.io/v3/' + infuraKey,
+			gas: 10000000,
+			accounts: { mnemonic: mnemonic }
+		},
+		testnet: {
+			url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+			gas: 10000000,
+			accounts: { mnemonic: mnemonic }
+		},
+		main: {
+			url: "https://bsc-dataseed.binance.org/",
+			gas: 10000000,
+			accounts: { mnemonic: mnemonic }
+		}
+	},
+	etherscan: {
+		apiKey: apiKey
+	}
 };
